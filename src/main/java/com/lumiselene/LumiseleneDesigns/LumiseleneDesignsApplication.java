ad72d5680af800e3b30dad4f7ac126e103fc5f8c
Kellsey Neagley <kellseyneagley@gmail.com>
2020-08-18 15:47:24 -0500

    generated spring application startup

---
+++ src/main/java/com/lumiselene/LumiseleneDesigns/LumiseleneDesignsApplication.java
@@ -0,0 +1,13 @@
+package com.lumiselene.LumiseleneDesigns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiseleneDesignsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiseleneDesignsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumiselene/LumiseleneDesigns/LumiseleneDesignsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumiselene.LumiseleneDesigns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumiseleneDesignsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
